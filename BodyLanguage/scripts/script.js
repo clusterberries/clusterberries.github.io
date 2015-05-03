b55afdc69da6dd668504189595739404f0ce7c8f
Alexandra Govorkova <al.govorkova@gmail.com>
2015-05-04 01:21:44 +0300

    Try history (still unsuccessful)

--- BodyLanguage/scripts/script.js
+++ BodyLanguage/scripts/script.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){  
-    $('.newsLink').click(function(){  
+$(document).ready(function(){    
+
+    $('.mainLink a').click(function(e) {
+        e.preventDefault();
+        $.ajax({  
+            url: "../index.html",  
+            success: function(html){  
+                $("main").html($(e).find('main').html());    
+            } 
+        }); 
+    });
+
+    $('.newsLink a').click(function(e){  
         $.ajax({  
             url: "../news.html",  
             success: function(html){  
-                $("main").html(html);  
+                $("main").html($(e).find('main').html());  
             }  
         });  
     });  
