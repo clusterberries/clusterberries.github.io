91aceeffa8a90169a79601b0d61326493c21da38
Alexandra Govorkova <al.govorkova@gmail.com>
2015-05-04 02:19:32 +0300

    Ajax with history is working

--- BodyLanguage/scripts/script.js
+++ BodyLanguage/scripts/script.js
@@ -1,22 +1,54 @@
 $(document).ready(function(){    
+	// получаем нормальный объект Location
+	var location = window.history.location || window.location;
 
-    $('.mainLink a').click(function(e) {
-        e.preventDefault();
+    $('.link a').click(function(e){ 
+    	// заносим ссылку в историю
+	    history.pushState(null, null, this.href); 
+
         $.ajax({  
-            url: "../index.html",  
+            url: ".." + this.pathname, 
+            success: function(html){  
+                $("main").html($(html).filter('main').html());  
+            }  
+        });  
+        // prevent default
+        return false;
+    });  
+
+    $(window).on('popstate', function(e) {
+      	history.location = location.href;
+    });
+            
+});  
+
+/*$(function() {
+        // получаем нормальный объект Location
+        var location = window.history.location || window.location;
+
+        // ищем все ссылки и вешаем события на все ссылки в нашем документе
+        $('a.ajax').click(function(e){  
+          // заносим ссылку в историю
+          history.pushState(null, null, this.href);
+
+          $.ajax({  
+            url: ".." + this.href,  
             success: function(html){  
                 $("main").html($(e).find('main').html());    
             } 
-        }); 
-    });
+          }); 
+          // тут можете вызвать подгрузку данных и т.п.
 
-    $('.newsLink a').click(function(e){  
-        $.ajax({  
-            url: "../news.html",  
-            success: function(html){  
-                $("main").html($(e).find('main').html());  
-            }  
-        });  
-    });  
-            
-});  
+          // не даем выполнить действие по умолчанию
+          return false;
+        });
+
+        // вешаем событие на popstate которое срабатывает при нажатии back/forward в браузере
+        $(window).on('popstate', function(e) {
+
+          // тут можете вызвать подгрузку данных и т.п.
+
+          // просто сообщение
+          alert("Мы вернулись на страницу со ссылкой: " + location.href);
+        });
+      });*/
